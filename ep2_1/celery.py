90e465c770e8e34b4a9232aa7220588e78c29383
mykola <nikolay.pogoreliy@gmail.com>
2021-05-30 11:12:55 +0300

    celery tasks working

--- ep2_1/celery.py
+++ ep2_1/celery.py
@@ -1,22 +1,21 @@
-import logging
+import json
 import os
 
-import celery
+from asgiref.sync import async_to_sync
 from celery import Celery
+from celery.utils.log import get_task_logger
+from channels.layers import get_channel_layer
+
+from services.bitmex_ws_client import BitmexWsClient
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ep2_1.settings')
 
 celery_app = Celery('ep2_1')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 # celery_app.autodiscover_tasks()
+ws_list = []
 
-
-@celery.signals.after_setup_logger.connect
-def on_after_setup_logger(**kwargs):
-    logger = logging.getLogger('celery')
-    logger.propagate = True
-    logger = logging.getLogger('celery.app.trace')
-    logger.propagate = True
+logger = get_task_logger(__name__)
 
 
 @celery_app.on_after_configure.connect
@@ -27,24 +26,15 @@ def setup_periodic_tasks(sender, **kwargs):
 @celery_app.task
 def get_report():
     # logger.debug('Start task')
-    raise Exception
-    print("AZAZAZ")
     channel_layer = get_channel_layer()
     if not ws_list:
         ws_list.append(BitmexWsClient())
     results = ws_list[0].get_instrument()
-    print(id(ws_list[0]))
-    # logger.debug(results)
     data = {
         'timestamp': results.get('timestamp'),
         'symbol': results.get('symbol'),
         'price': results.get('lastPrice')
     }
-    # logger.debug(data)
-    async_to_sync(channel_layer.group_send)(
-        "bitmex_feed",
-        {
-            "type": "bitmex_feed.feed_message",
-            "message": json.dumps(data),
-        },
-    )
+    logger.debug(data)
+    async_to_sync(channel_layer.group_send)("bitmex_feed", {
+        "type": "bitmex_feed.feed_message", "message": json.dumps(data), }, )
